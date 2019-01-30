eb1dad1b5b165ef0f35690987781618d8edeb824
mpartee123 <m_partee@hotmail.com>
2019-01-29 19:43:58 -0600

    added css styling still needs media Queery

--- assets/app.js
+++ assets/app.js
@@ -4,13 +4,11 @@
 var url = "https://api.giphy.com/v1/gifs/search";
 var apiKey = "VFw1T2oLjjK9zYZYbU9BM10HO1g1xqWE";
 
-var searchTerms = ['cow', 'cat', 'dog','chicken','sheep', 'turkey','duck','horse','pig', 'donkey','rabbit'];
+var searchTerms = ['cow', 'cat', 'dog','chicken','sheep', 'turkey','duck','horse','pig', 'donkey'];
 
 // first thing buttons are populated from pre defined array of search terms
 function init() {
     renderButtons(searchTerms);
-    // localStorage.setItem('searchTerms', searchTerms );
-
 }
 
 function getGifs(searchTerm) {
@@ -19,10 +17,12 @@ function getGifs(searchTerm) {
     $.ajax(url, {
         data: {
             'apiKey': apiKey,
+            // 'q': "farm " + searchTerm,
             'q': searchTerm,
             'limit': 10,
             'lan': 'en',
         }
+
     }).then(function (response) {
         $('#gifs').empty();
 
@@ -33,9 +33,10 @@ function getGifs(searchTerm) {
             var rating = gif.rating;
             renderGif(staticUrl, animatedUrl, rating);
         });
-    }).catch(function () {
-
-    });
+    })
+//         .catch(function (gif) {
+// alert("Im sorry I was not able to get your Gifs")
+//     });
 }
 
 function renderGif(staticUrl, animatedUrl, rating) {
@@ -44,8 +45,10 @@ function renderGif(staticUrl, animatedUrl, rating) {
     image.data('static', staticUrl);
     image.data('animated', animatedUrl);
     image.data('is-static', 'true');
-    var container = $('<div>');
-    container.append(rating);
+    var container = $('<div class="p">');
+    var header = $('<h3 class="rating">');
+    header.append("Rating: " + rating.toUpperCase());
+    container.append(header);
     container.append(image);
     $('#gifs').append(container);
     image.click(function () {
@@ -67,32 +70,46 @@ function renderGif(staticUrl, animatedUrl, rating) {
 
 function renderButtons(searchTerms) {
     // $('#buttons').empty();
-    // searchTerms = localStorage.getItem("searchTerms");
+    /**
+     * if (!Array.isArray(searchTerms)) {
+        list = [];
+    }
+
+     searchTerms = JSON.parse(localStorage.getItem("searchTerms"));
+     */
+
     searchTerms.forEach(function (searchTerm) {
-        var button = $('<button>');
-        button.html(searchTerm);
+        var button = $('<button class="search-term">');
+        button.html(searchTerm.toUpperCase());
 
         button.click(function (event) {
             getGifs(event.target.innerHTML);
         });
 
         $('#buttons').append(button);
+
     });
 }
+
 // add a submit listner that posts search input into array to make button.
 $("#submit-button").on("click", function(event) {
-    event.preventDefault();
-    // This line grabs the input from the textbox
-    var newGif = $("#input").val().trim();
-console.log(newGif);
-    // Adding search term from the textbox to searchTerm array
-    searchTerms.push(newGif);
-console.log(searchTerms);
-    $('#buttons').empty();
-    $('#input').val('');
-    // Calling renderButtons which handles the processing of gif searchTerms array
-    localStorage.setItem('searchTerms', searchTerms );
-    renderButtons(searchTerms);
+    if( $('#input').val() !== ''){
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var newGif = $("#input").val().trim();
+        console.log(newGif);
+        // Adding search term from the textbox to searchTerm array
+        searchTerms.push(newGif);
+        console.log(searchTerms);
+        $('#buttons').empty();
+        $('#input').val('');
+        // Calling renderButtons which handles the processing of gif searchTerms array
+        localStorage.setItem("searchTerms", JSON.stringify(searchTerms));
+        renderButtons(searchTerms);
+    } else{
+        alert("Please enter text into this field");
+        renderButtons(searchTerms);
+    }
 });
 
 $(document).ready(function () {
